e93e3b2ea6d3cf02a433c1ccc4a0cdf4ebfe8944
UnknownUser-Luko <153364408+UnknownUser-Luko@users.noreply.github.com>
2024-02-27 22:58:16 +0000

    x

---
+++ README.md
@@ -0,0 +1,2 @@
+# WebsubM
+x
---
+++ Sub script/script.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const header = document.querySelector("header");
+    header.addEventListener("click", function() {
+        alert("You clicked the header!");
+    });
+});
